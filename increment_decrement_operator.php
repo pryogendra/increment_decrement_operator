3769251e42f0043ba4c2ede71807c0bf72e68eea
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2023-03-06 08:51:28 +0530

    Update increment_decrement_operator.php

--- increment_decrement_operator.php
+++ increment_decrement_operator.php
@@ -4,13 +4,13 @@
     echo ++$a."<br>";  //Pre Increment Operator
     echo $a."<br>";
     $a=20;
-    echo --$a."<br>";   //Pre Decremenr Opertor
+    echo --$a."<br>";   //Pre Decrement Opertor
     echo $a."<br>";
     $a=30;
     echo $a++."<br>";  // Increment Operator
     echo $a."<br>";
     $a=40;
-    echo $a--."<br>";   // Decremenr Opertor
+    echo $a--."<br>";   // Decrement Opertor
     echo $a;
     ?>
 </html>
