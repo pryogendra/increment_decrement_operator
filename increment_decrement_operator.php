d2d9ca53b84e094bb813ae54ebe1f051cb6c635c
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2023-03-06 08:48:01 +0530

    Add files via upload

---
+++ increment_decrement_operator.php
@@ -0,0 +1,16 @@
+<html>
+    <?php
+    $a=10;
+    echo ++$a."<br>";  //Pre Increment Operator
+    echo $a."<br>";
+    $a=20;
+    echo --$a."<br>";   //Pre Decremenr Opertor
+    echo $a."<br>";
+    $a=30;
+    echo $a++."<br>";  // Increment Operator
+    echo $a."<br>";
+    $a=40;
+    echo $a--."<br>";   // Decremenr Opertor
+    echo $a;
+    ?>
+</html>
